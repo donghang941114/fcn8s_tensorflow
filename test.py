44deba8daf305c9c87d70f7da43ac52a26c0e214
donghang <1746314075@qq.com>
2019-01-11 12:21:42 +0800

    new

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from cityscapesscripts.helpers.labels import TRAINIDS_TO_COLORS_DICT, TRAINIDS_T
 from math import ceil
 import time
 import matplotlib.pyplot as plt
+import cv2
 
 # TODO: Set the paths to the images.
 train_images = '../datasets/data/leftImg8bit/train/'
@@ -106,3 +107,52 @@ cells[1].imshow(example_gt_images[i])
 plt.figure(figsize=(16, 8))
 plt.imshow(example_gt_images[i])
 cv2.imwrite("../result_data",example_gt_images[i])
+
+
+#Create the model
+model = FCN8s(model_load_dir=None,
+              tags=None,
+              vgg16_dir='../VGG-16_mod2FCN_ImageNet-Classification',
+              num_classes=num_classes,
+              variables_load_dir=None)
+
+#Train the model
+epochs = 6  # TODO: Set the number of epochs to train for.
+
+
+# TODO: Define a learning rate schedule function to be passed to the `train()` method.
+def learning_rate_schedule(step):
+    if step <= 10000:
+        return 0.0001
+    elif 10000 < step <= 20000:
+        return 0.00001
+    elif 20000 < step <= 40000:
+        return 0.000003
+    else:
+        return 0.000001
+
+
+model.train(train_generator=train_generator,
+            epochs=epochs,
+            steps_per_epoch=ceil(num_train_images / batch_size),
+            learning_rate_schedule=learning_rate_schedule,
+            keep_prob=0.5,
+            l2_regularization=0.0,
+            eval_dataset='val',
+            eval_frequency=2,
+            val_generator=val_generator,
+            val_steps=ceil(num_val_images / batch_size),
+            metrics={'loss', 'mean_iou', 'accuracy'},
+            save_during_training=True,
+            save_dir='cityscapes_model',
+            save_best_only=True,
+            save_tags=['default'],
+            save_name='(batch-size-4)',
+            save_frequency=2,
+            saver='saved_model',
+            monitor='loss',
+            record_summaries=True,
+            summaries_frequency=10,
+            summaries_dir='tensorboard_log/cityscapes',
+            summaries_name='configuration_01',
+            training_loss_display_averaging=3)
